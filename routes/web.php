0696ed4008fefe622f5abd0407ded5617e35abc9
rendijsberkis <rendijsberkis7@gmail.com>
2025-12-09 10:35:02 +0200

    floor images, floor information endpoint as well as filled property seeder

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,43 @@
 
 use App\Http\Controllers\Admin\AuthController;
 use App\Http\Controllers\Admin\PropertyController;
+use App\Models\Property;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/api/floors', function () {
+    $properties = Property::all();
+
+    $floorData = $properties->groupBy('floor')->map(function ($properties, $floor) {
+        $availableCount = $properties->where('status', 'available')->count();
+        $minPrice = $properties->min('price');
+        $maxPrice = $properties->max('price');
+
+        return [
+            'level' => $floor,
+            'total_units' => $properties->count(),
+            'available_units' => $availableCount,
+            'min_price' => $minPrice,
+            'max_price' => $maxPrice,
+            'properties' => $properties->map(function ($property) {
+                return [
+                    'id' => $property->id,
+                    'flat_number' => $property->flat_number,
+                    'flat_area' => $property->flat_area,
+                    'total_area' => $property->total_area,
+                    'terrace_area' => $property->terrace_area,
+                    'balcony_area' => $property->balcony_area,
+                    'price' => $property->price,
+                    'status' => $property->status,
+                ];
+            })->values(),
+        ];
+    })->sortKeys()->values();
+
+    return response()->json($floorData);
 });
 
 Route::middleware('guest')->group(function () {
